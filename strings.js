939af334e7bb940aa404cb7868209614630f20b0
Harmony Chofor <jethroharmony11@gmail.com>
2023-10-24 13:11:37 -0500

    function

---
+++ strings.js
@@ -0,0 +1,15 @@
+
+
+let test = "Nina's dog."
+
+let teacher = 'kevin says JUMP"!!!'
+
+//\ "literal" - backslash is the ESCAPE character
+let text2 = "kevin says \"JUMP\"!!!"
+
+//MODERN with black ticks - template string ${expr}
+let text3 = `Nina's dog.`
+
+let teacher3 = `kevin says "JUMP"`
+
+console.log(`\\`); //literal backslash
